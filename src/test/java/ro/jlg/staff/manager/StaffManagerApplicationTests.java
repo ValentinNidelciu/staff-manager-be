562faf2bd476b2bd6b1ca6eb41ea9f53c6183d91
unknown <valentin.nidelciu@gmail.com>
2022-08-01 14:37:46 +0300

    initial commit

---
+++ src/test/java/ro/jlg/staff/manager/StaffManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.jlg.staff.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaffManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
